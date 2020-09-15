592aa5904310b5bc6cbdb1a8da7ebe506930396b
Krzysztof Gorecki <kg@b-intohimo.com>
2020-09-15 12:16:34 +0300

    some cleaning

--- Calc2Numbers.java
+++ Calc2Numbers.java
@@ -7,20 +7,17 @@ public class Calc2Numbers {
         final int number1 = user_input("1st");
         final int number2 = user_input("2nd");
 
-        final int returned_Total = add2Numbers(number1, number2);
-
-        System.out.println("The total for your two numbers is " + returned_Total);
+        System.out.println("The total for your two numbers is "
+            + add2Numbers(number1, number2));
     }
 
     public static int user_input(final String whatNumber) {
         input = new Scanner(System.in);
         System.out.print("Enter the " + whatNumber + " number: ");
-        final int num1 = input.nextInt();
-        return num1;
+        return input.nextInt();
     }
 
     public static int add2Numbers(final int num1, final int num2) {
-        final int total = num1 + num2;
-        return total;
+        return num1 + num2;
     }
 }
